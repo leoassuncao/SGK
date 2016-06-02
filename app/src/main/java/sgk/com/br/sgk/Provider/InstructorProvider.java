3ecab99349b0062976c60ec36ae6875a7e040438
Leonardo Vinicius de Assuncao Sousa Filh <f764542@corp.caixa.gov.br>
2016-06-02 11:44:00 -0300

    minor layout changes

--- app/src/main/java/sgk/com/br/sgk/Provider/InstructorProvider.java
+++ app/src/main/java/sgk/com/br/sgk/Provider/InstructorProvider.java
@@ -30,10 +30,10 @@ public class InstructorProvider {
         InstructorsList tiago = new InstructorsList();
 
         tiago.setInstructor_photo(R.drawable.tiago_porto);
-        tiago.setInstructor_name("Sensei Tiago França (SGK/JKA)\n");
-        tiago.setInstructor_graduation("Graduação: 4º Dan (JKA)\n");
-        tiago.setInstructor_train("Onde Treina: Julio Adnet (AJA)\n");
-        tiago.setInstructor_classes("Onde dá Aulas: AJA e Cia Athletica\n");
+        tiago.setInstructor_name("Sensei Tiago França (SGK/JKA)");
+        tiago.setInstructor_graduation("4º Dan (JKA)");
+        tiago.setInstructor_train("Onde Treina: Julio Adnet (AJA)");
+        tiago.setInstructor_classes("Onde dá Aulas: AJA e Cia Athletica");
         tiago.setInstructor_contact("tiago.franca@sgkdojo.com.br");
 
         return tiago;
@@ -43,10 +43,10 @@ public class InstructorProvider {
         InstructorsList rafael = new InstructorsList();
 
         rafael.setInstructor_photo(R.drawable.rafael_zardo);
-        rafael.setInstructor_name("Sensei Rafael Zardo (SGK)\n");
-        rafael.setInstructor_graduation("Graduação: 2º Dan (JKA)\n");
-        rafael.setInstructor_train("Onde Treina: Julio Adnet (AJA)\n");
-        rafael.setInstructor_classes("Onde dá Aulas: Julio Adnet (AJA)\n");
+        rafael.setInstructor_name("Sensei Rafael Zardo (SGK)");
+        rafael.setInstructor_graduation("2º Dan (JKA)");
+        rafael.setInstructor_train("Onde Treina: Julio Adnet (AJA)");
+        rafael.setInstructor_classes("Onde dá Aulas: Julio Adnet (AJA)");
         rafael.setInstructor_contact("rafael.zardo@sgkdojo.com.br");
 
         return rafael;
@@ -56,10 +56,10 @@ public class InstructorProvider {
         InstructorsList gabriel = new InstructorsList();
 
         gabriel.setInstructor_photo(R.drawable.gabriel_alonzo);
-        gabriel.setInstructor_name("Sensei Gabriel Alonzo (SGK)\n");
-        gabriel.setInstructor_graduation("Graduação: 2º Dan (JKA)\n");
-        gabriel.setInstructor_train("Onde Treina: Julio Adnet (AJA)\n");
-        gabriel.setInstructor_classes("Onde dá Aulas:UnB\n");
+        gabriel.setInstructor_name("Sensei Gabriel Alonzo (SGK)");
+        gabriel.setInstructor_graduation("2º Dan (JKA)");
+        gabriel.setInstructor_train("Onde Treina: Julio Adnet (AJA)");
+        gabriel.setInstructor_classes("Onde dá Aulas:UnB");
         gabriel.setInstructor_contact("gabriel.alonzo@sgkdojo.com.br");
 
         return gabriel;
@@ -69,9 +69,9 @@ public class InstructorProvider {
         InstructorsList everton = new InstructorsList();
 
         everton.setInstructor_photo(R.drawable.everton_rodrigues);
-        everton.setInstructor_name("Sensei Everton Rodrigues (SGK)\n");
-        everton.setInstructor_graduation("Graduação: 2º Dan (JKA)\n");
-        everton.setInstructor_train("Onde Treina: Natal-RN\n");
+        everton.setInstructor_name("Sensei Everton Rodrigues (SGK)");
+        everton.setInstructor_graduation("2º Dan (JKA)");
+        everton.setInstructor_train("Onde Treina: Natal-RN");
         everton.setInstructor_classes("everton.rodrigues@sgkdojo.com.br");
 
         return everton;
@@ -82,9 +82,9 @@ public class InstructorProvider {
 
         cristiano.setInstructor_photo(R.drawable.cristiano_morais);
         cristiano.setInstructor_name("Sensei Cristiano Morais (SGK)");
-        cristiano.setInstructor_graduation("Graduação: 2º Dan (JKA)\n");
-        cristiano.setInstructor_train("Onde Treina: Julio Adnet (AJA)\n");
-        cristiano.setInstructor_classes("Onde dá Aulas: Templo-(Infantil)\n");
+        cristiano.setInstructor_graduation("2º Dan (JKA)");
+        cristiano.setInstructor_train("Onde Treina: Julio Adnet (AJA)");
+        cristiano.setInstructor_classes("Onde dá Aulas: Templo-(Infantil)");
         cristiano.setInstructor_contact("cristiano.morais@sgkdojo.com.br");
 
         return cristiano;
@@ -94,10 +94,10 @@ public class InstructorProvider {
         InstructorsList zuza = new InstructorsList();
 
         zuza.setInstructor_photo(R.drawable.guilherme_zuza);
-        zuza.setInstructor_name("Sensei Guilherme Zuza (SGK)\n");
-        zuza.setInstructor_graduation("Graduação: 2º Dan (JKA)\n");
-        zuza.setInstructor_train("Onde Treina: Julio Adnet (AJA)\n");
-        zuza.setInstructor_classes("Onde dá Aulas: Templo-(Adulto)\n");
+        zuza.setInstructor_name("Sensei Guilherme Zuza (SGK)");
+        zuza.setInstructor_graduation("2º Dan (JKA)");
+        zuza.setInstructor_train("Onde Treina: Julio Adnet (AJA)");
+        zuza.setInstructor_classes("Onde dá Aulas: Templo-(Adulto)");
         zuza.setInstructor_contact("guilherme.zuza@sgkdojo.com.br");
 
         return zuza;
@@ -107,9 +107,9 @@ public class InstructorProvider {
         InstructorsList martins = new InstructorsList();
 
         martins.setInstructor_photo(R.drawable.rafael_martins);
-        martins.setInstructor_name("Sensei Rafael Martins (SGK)\n");
-        martins.setInstructor_graduation("Graduação: 1º Dan (JKA)\n");
-        martins.setInstructor_train("Onde dá Aulas: Ippon\n");
+        martins.setInstructor_name("Sensei Rafael Martins (SGK)");
+        martins.setInstructor_graduation("1º Dan (JKA)");
+        martins.setInstructor_train("Onde dá Aulas: Ippon");
         martins.setInstructor_classes("rafael.martins@sgkdojo.com.br");
 
         return martins;
@@ -119,9 +119,9 @@ public class InstructorProvider {
         InstructorsList amanda = new InstructorsList();
 
         amanda.setInstructor_photo(R.drawable.amanda_gontijo);
-        amanda.setInstructor_name("Sensei Amanda Gontijo (SGK)\n");
-        amanda.setInstructor_graduation("Graduação: 1º Dan (JKA)\n");
-        amanda.setInstructor_train("Onde Treina: Julio Adnet (AJA)\n");
+        amanda.setInstructor_name("Sensei Amanda Gontijo (SGK)");
+        amanda.setInstructor_graduation("1º Dan (JKA)");
+        amanda.setInstructor_train("Onde Treina: Julio Adnet (AJA)");
         amanda.setInstructor_classes("amanda.gontijo@sgkdojo.com.br");
 
         return amanda;
